077e087ecf30f890d220b00e0caa4ae95972bb3a
dazeez1 <d.azeez@alustudent.com>
2024-02-29 06:27:14 -0800

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""""Doc"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-
-    res = requests.get(url,
-                       headers={
-                           'User-Agent': 'Mozilla/5.0'})
-
-    if res.status_code != 200:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    else:
-        json_response = res.json()
-        return json_response.get('data').get('subscribers')
+    results = response.json().get("data")
+    return results.get("subscribers")
